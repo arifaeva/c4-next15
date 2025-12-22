baa76b057d11953a4cf42fea509a65763cabb4a1
arifaeva <arifa.eva.ixa@gmail.com>
2025-12-22 19:56:20 +0700

    feat(dashboard): add chartOrder component and ChartOrderSkeleton to dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import ChartOrderSkeleton from "./@chartOrder/chartOrderSkeleton";
 
 interface LayoutProps {
+  chartOrder: React.ReactNode;
   compA: React.ReactNode;
   compB: React.ReactNode;
   children: React.ReactNode;
@@ -9,6 +11,12 @@ interface LayoutProps {
 export default function Layout(props: LayoutProps) {
   return (
     <main>
+      <section>
+        <React.Suspense fallback={<ChartOrderSkeleton />}>
+          <div>{props.chartOrder}</div>
+        </React.Suspense>
+      </section>
+
       <section className="grid grid-cols-2">
         <React.Suspense fallback={<div>Loading Comp A...</div>}>
           <div>{props.compA}</div>
@@ -17,6 +25,7 @@ export default function Layout(props: LayoutProps) {
           <div>{props.compB}</div>
         </React.Suspense>
       </section>
+
       <section>
         <React.Suspense fallback={<div>Loading Children...</div>}>
           <div>{props.children}</div>
