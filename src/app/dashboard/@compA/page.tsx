26bff034bca4c2263ff9b665fa2ed638760952ac
arifaeva <arifa.eva.ixa@gmail.com>
2025-12-22 20:02:05 +0700

    feat(dashboard): add math random function and its error handling in compA component

--- src/app/dashboard/@compA/page.tsx
+++ src/app/dashboard/@compA/page.tsx
@@ -1,3 +1,9 @@
 export default function CompA() {
+  const randomBoolean = Math.random() < 0.5;
+
+  if (randomBoolean) {
+    throw new Error("Something went wrong");
+  }
+
   return <div>Comp A</div>;
 }
