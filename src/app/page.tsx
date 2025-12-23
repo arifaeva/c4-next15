0d8f241a1e101f11ca4c885be04c06ee65241f92
arifaeva <arifa.eva.ixa@gmail.com>
2025-12-23 23:35:07 +0700

    feat(homepage): render note database using Prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import { ClientComp2 } from "@/components/client-comp-2";
+import { prisma } from "@/utils/prisma";
 
 export default async function Page() {
+  const notes = await prisma.note.findMany();
+
   return (
     <div>
-      <ClientComp2 />
+      <h3>Notes</h3>
+      <div>
+        {notes.map((note) => (
+          <div key={note.id}>{note.content}</div>
+        ))}
+      </div>
     </div>
   );
 }
