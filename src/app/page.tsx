7f71fc8157f62a62e6bfaff99b49395ba4decd2f
arifaeva <arifa.eva.ixa@gmail.com>
2025-12-24 11:34:33 +0700

    feat(homepage): add note links and create note form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import { prisma } from "@/utils/prisma";
+import { createNote } from "./action";
+import Link from "next/link";
 
 export default async function Page() {
   const notes = await prisma.note.findMany();
@@ -8,8 +10,16 @@ export default async function Page() {
       <h3>Notes</h3>
       <div>
         {notes.map((note) => (
-          <div key={note.id}>{note.content}</div>
+          <Link href={`/${note.id}`} key={note.id} className="block">
+            {note.content}
+          </Link>
         ))}
+      </div>
+      <div>
+        <form action={createNote}>
+          <input name="content" placeholder="Note..." />
+          <button>Save</button>
+        </form>
       </div>
     </div>
   );
