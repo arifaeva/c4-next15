778db4f48d330667d9760a8fb37befb23ea48520
arifaeva <arifa.eva.ixa@gmail.com>
2025-12-26 21:47:03 +0700

    style(actions): display error message from server action

--- src/new-hooks/useActionState.tsx
+++ src/new-hooks/useActionState.tsx
@@ -17,12 +17,16 @@ export const NextActionState = () => {
     <div>
       <form action={formAction} className="space-y-4">
         <h3>useActionState</h3>
-        <p>
+        <p className="text-gray-500">
           Use Action State is the way we can abstract the submisssion status and
           handling return
         </p>
-        <input name="content" placeholder="Note..." />
+        <input name="content" placeholder="Note..." className="block" />
         <button disabled={isPending}>Save</button>
+
+        {state?.status === "error" && (
+          <div className="text-red-500">{state.message}</div>
+        )}
       </form>
     </div>
   );
