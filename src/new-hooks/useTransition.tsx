8163227d81ccfab2747cdc0f6e6a2aee748aa063
arifaeva <arifa.eva.ixa@gmail.com>
2026-01-06 22:03:58 +0700

    refactor(hooks): clarify useTransition example

--- src/new-hooks/useTransition.tsx
+++ src/new-hooks/useTransition.tsx
@@ -1,3 +1,5 @@
+// WITH useTransition----------------------------------------------------------------------------------------------------------------------------------------
+
 "use client";
 
 import React from "react";
@@ -12,11 +14,16 @@ export const NextUseTransition = ({ items }: { items: Item[] }) => {
   const [renderedItems, setRenderedItems] = React.useState<Item[]>([]);
   const [isPending, startTransition] = React.useTransition();
   const handleToggleSidebar = () => {
-    setSidebarVisible((prev) => !prev);
+    // Marked as Urgent
+    // set Sidebar Visible
+    setSidebarVisible((prev) => !prev); // cheap
 
+    // Marked as non-urgent
     startTransition(() => {
+      // set item yang di-render
       if (!sidebarVisible) {
         setRenderedItems(items); // if sidebar is opening, load the items
+        // expensive, 50.000 item in array
       } else {
         setRenderedItems([]); // if closing, remove the items
       }
@@ -45,3 +52,46 @@ export const NextUseTransition = ({ items }: { items: Item[] }) => {
     </div>
   );
 };
+
+// WITHOUT useTransition----------------------------------------------------------------------------------------------------------------------------------------
+
+// "use client";
+
+// import React from "react";
+
+// export interface Item {
+//   id: string;
+//   name: string;
+// }
+
+// export const NextUseTransition = ({ items }: { items: Item[] }) => {
+//   const [sidebarVisible, setSidebarVisible] = React.useState(false);
+//   const [renderedItems, setRenderedItems] = React.useState<Item[]>([]);
+//   const handleToggleSidebar = () => {
+//     setSidebarVisible((prev) => !prev);
+
+//     if (!sidebarVisible) {
+//       setRenderedItems(items); // if sidebar is opening, load the items
+//     } else {
+//       setRenderedItems([]); // if closing, remove the items
+//     }
+//   };
+
+//   return (
+//     <div className="max-w-lg m-auto py-12">
+//       <button onClick={handleToggleSidebar}>
+//         {sidebarVisible ? "Hide Users" : "Show Users"}
+//       </button>
+
+//       {sidebarVisible && (
+//         <div className="sidebar">
+//           <ul>
+//             {renderedItems.map((item) => (
+//               <li key={item.id}>{item.name}</li>
+//             ))}
+//           </ul>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
